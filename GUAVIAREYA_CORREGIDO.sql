ac5354935024973da9a38e36a402616f58fbb504
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-06-27 06:53:38 -0500

    Add files via upload

--- GUAVIAREYA_CORREGIDO.sql
+++ GUAVIAREYA_CORREGIDO.sql
@@ -4,7 +4,14 @@ USE bd_guaviareya;
 
 -- Creación de tablas
 
--- Creación de tablas
+CREATE TABLE administrador(
+    correo VARCHAR(50) NOT NULL PRIMARY KEY,
+    apodo VARCHAR(50) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL
+);
+
+select * from administrador;
+
 CREATE TABLE Usuarios (
     Correo VARCHAR(50) NOT NULL PRIMARY KEY,
     Apodo VARCHAR(50) NOT NULL,
@@ -13,39 +20,34 @@ CREATE TABLE Usuarios (
     Contrasena VARCHAR(50) NOT NULL,
     Telefono VARCHAR(15) NOT NULL,
     Fec_Regis DATETIME NOT NULL,
-    img_U LONGBLOB NOT NULL
+    img_U VARCHAR(200) NOT NULL
 );
-
-select * from Usuarios;
-/*ALTER TABLE Usuarios
-ADD Apodo VARCHAR(50) NOT NULL AFTER ID_User;*/
 
 CREATE TABLE Restaurantes (
     ID_Restaurante INT NOT NULL PRIMARY KEY,
-    ID_Producto INT NOT NULL,
     Nombre_R VARCHAR(50) NOT NULL,
-    Dirrecion VARCHAR(50) NOT NULL,
+    Direccion VARCHAR(50) NOT NULL,
     Telefono VARCHAR(15) NOT NULL,
-    img_R LONGBLOB NOT NULL
+    img_R VARCHAR(200) NOT NULL
 );
-
-
 
 CREATE TABLE Productos (
     ID_Producto INT NOT NULL PRIMARY KEY,
     ID_Restaurante INT NOT NULL,
     Nombre_P VARCHAR(50) NOT NULL,
-    Descripcion varchar (300) not null,
+    Descripcion VARCHAR(300) NOT NULL,
     Valor_P INT NOT NULL,
-    img_P LONGBLOB NOT NULL
+    img_P VARCHAR(200) NOT NULL,
+    CONSTRAINT FK_Restaurantes_Productos FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante)
 );
 
-CREATE TABLE Dirrecion_Entregas (
+CREATE TABLE Direccion_Entregas (
     ID_Dire_Entre INT NOT NULL PRIMARY KEY,
     Correo VARCHAR(50) NOT NULL,
     Numero_Casa VARCHAR(10) NOT NULL,
     CL_Cra_AV VARCHAR(50) NOT NULL,
-    Barrio VARCHAR(50) NOT NULL
+    Barrio VARCHAR(50) NOT NULL,
+    CONSTRAINT FK_Usuarios_Direccion_Entregas FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
 );
 
 CREATE TABLE Pagos (
@@ -54,7 +56,8 @@ CREATE TABLE Pagos (
     Tipo_Pago VARCHAR(20) NOT NULL,
     Monto_Final INT NOT NULL,
     Fecha_Pago DATETIME NOT NULL,
-    img_pa LONGBLOB  NULL
+    img_pa LONGBLOB NULL,
+    CONSTRAINT FK_Usuarios_Pagos FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
 );
 
 CREATE TABLE Domiciliarios (
@@ -62,128 +65,71 @@ CREATE TABLE Domiciliarios (
     Correo VARCHAR(50) NOT NULL,
     Nombre_Domiciliario VARCHAR(20) NOT NULL,
     Telefono_Domi VARCHAR(15) NOT NULL,
-    img_do LONGBLOB NOT NULL
+    img_do VARCHAR(200) NOT NULL,
+    CONSTRAINT FK_Usuarios_Domiciliarios FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
 );
 
 CREATE TABLE Pedidos (
     ID_pedido INT NOT NULL PRIMARY KEY,
-	ID_Restaurante INT NOT NULL,
+    ID_Restaurante INT NOT NULL,
     ID_Producto INT NOT NULL,
-    Descripcion varchar (300) not null,
-	cantidad INT NOT NULL,
-    Sub_total double not null
-);      
+    Descripcion VARCHAR(300) NOT NULL,
+    cantidad INT NOT NULL,
+    Sub_total DOUBLE NOT NULL,
+    CONSTRAINT FK_Productos_Pedidos FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto),
+    CONSTRAINT FK_Restaurantes_Pedidos FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante)
+);
 
 CREATE TABLE Pedidos_factura (
     ID_Pedifac INT NOT NULL PRIMARY KEY,
     ID_pedido INT NOT NULL,
     Correo VARCHAR(50) NOT NULL,
     ID_Restaurante INT NOT NULL,
-	ID_Producto INT NOT NULL,
-	ID_Dire_Entre INT NOT NULL,
-    ID_pago INT NOT NULL,
+    ID_Producto INT NOT NULL,
+    ID_Dire_Entre INT NOT NULL,
+    ID_Pago INT NOT NULL,
     ID_Domiciliario INT NOT NULL,
     Estado_Pedido VARCHAR(50) NOT NULL,
     Subtotal INT NOT NULL,
-    Valor_Domi INT NOT NULL default 5000,
-    Valor_Pagar int not null,
-    img_pef LONGBLOB NOT NULL
+    Valor_Domi INT NOT NULL DEFAULT 5000,
+    Valor_Pagar INT NOT NULL,
+    CONSTRAINT FK_Pedidos_Pedidos_factura FOREIGN KEY (ID_pedido) REFERENCES Pedidos (ID_pedido),
+    CONSTRAINT FK_Usuarios_Pedidos_factura FOREIGN KEY (Correo) REFERENCES Usuarios (Correo),
+    CONSTRAINT FK_Direccion_Entregas_Pedidos_factura FOREIGN KEY (ID_Dire_Entre) REFERENCES Direccion_Entregas (ID_Dire_Entre),
+    CONSTRAINT FK_Productos_Pedidos_factura FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto),
+    CONSTRAINT FK_Domiciliarios_Pedidos_factura FOREIGN KEY (ID_Domiciliario) REFERENCES Domiciliarios (ID_Domiciliario),
+    CONSTRAINT FK_Restaurantes_Pedidos_factura FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante),
+    CONSTRAINT FK_Pagos_Pedidos_factura FOREIGN KEY (ID_Pago) REFERENCES Pagos (ID_Pago)
 );
 
--- CONSTRAINTS
-
-
-ALTER TABLE Productos
-ADD CONSTRAINT FK_Restaurantes_Productos
-FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante);
-
-ALTER TABLE Pedidos
-ADD CONSTRAINT FK_Productos_Pedidos
-FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto);
-
-ALTER TABLE Pedidos
-ADD CONSTRAINT FK_Restaurantes_Pedidos
-FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Pedidos_Pedidos_factura
-FOREIGN KEY (ID_pedido) REFERENCES Pedidos (ID_pedido);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Usuarios_Pedidos_factura
-FOREIGN KEY (Correo) REFERENCES Usuarios (Correo);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Dirrecion_Entregas_Pedidos_factura
-FOREIGN KEY (ID_Dire_Entre) REFERENCES Dirrecion_Entregas (ID_Dire_Entre);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Pedidos_Pedidos_factura
-FOREIGN KEY (ID_Producto) REFERENCES Dirrecion_Entregas (ID_Producto);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Domiciliarios_Pedidos_factura
-FOREIGN KEY (ID_Domiciliario) REFERENCES Domiciliarios (ID_Domiciliario);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Restaurantes_Pedidos_factura
-FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante);
-
-ALTER TABLE Pedidos_factura
-ADD CONSTRAINT FK_Pagos_Pedidos_factura
-FOREIGN KEY (ID_Pago) REFERENCES Pagos (ID_Pago);
-
-ALTER TABLE Dirrecion_Entregas
-ADD CONSTRAINT FK_Usuarios_Dirrecion_Entregas
-FOREIGN KEY (Correo) REFERENCES Usuarios (Correo);
-
-ALTER TABLE Pagos
-ADD CONSTRAINT FK_Usuarios_Pagos
-FOREIGN KEY (Correo) REFERENCES Usuarios (Correo);
-
-ALTER TABLE Domiciliarios
-ADD CONSTRAINT FK_Usuarios_Domiciliarios
-FOREIGN KEY (Correo) REFERENCES Usuarios (Correo);
-
-
-/*
 -- Joins y selects
 
 -- Join usuario-pago
 SELECT * FROM Usuarios t1, Pagos t2
-WHERE t2.correo = t1.correo;
+WHERE t2.Correo = t1.Correo;
 
 -- Join de Usuario - n° pedidos
-SELECT Usuarios.correo, Usuarios.Nombre, Usuarios.Apellido, COUNT(Pedidos.ID_pedido) AS Num_Pedidos
-FROM Usuarios, Pedidos
-WHERE Usuarios.correo = Pedidos.correo
-GROUP BY Usuarios.correo, Usuarios.Nombre, Usuarios.Apellido;
-
+SELECT Usuarios.Correo, Usuarios.Nombre, Usuarios.Apellido, COUNT(Pedidos.ID_pedido) AS Num_Pedidos
+FROM Usuarios
+JOIN Pedidos ON Usuarios.Correo = Pedidos.Correo
+GROUP BY Usuarios.Correo, Usuarios.Nombre, Usuarios.Apellido;
 
 -- Seleccionar todas las tablas
 SELECT * FROM Usuarios;
 SELECT * FROM Restaurantes;
 SELECT * FROM Productos;
-SELECT * FROM Dirrecion_Entregas;
+SELECT * FROM Direccion_Entregas;
 SELECT * FROM Domiciliarios;
 SELECT * FROM Pagos;
 SELECT * FROM Pedidos;
-select * from Pedidos_factura;
-
-
-
-
-
-
-
-
+SELECT * FROM Pedidos_factura;
 
 -- Drop de todas las tablas
-#drop table if exists Pedidos_factura;
-#DROP TABLE IF EXISTS Pedidos;
-#DROP TABLE IF EXISTS Domiciliarios;
-#DROP TABLE IF EXISTS Dirrecion_Entregas;
-#DROP TABLE IF EXISTS Productos;
-#DROP TABLE IF EXISTS Restaurantes;
-#DROP TABLE IF EXISTS Pagos;
-#DROP TABLE IF EXISTS Usuarios;
+-- DROP TABLE IF EXISTS Pedidos_factura;
+-- DROP TABLE IF EXISTS Pedidos;
+-- DROP TABLE IF EXISTS Domiciliarios;
+-- DROP TABLE IF EXISTS Direccion_Entregas;
+-- DROP TABLE IF EXISTS Productos;
+-- DROP TABLE IF EXISTS Restaurantes;
+-- DROP TABLE IF EXISTS Pagos;
+-- DROP TABLE IF EXISTS Usuarios;
