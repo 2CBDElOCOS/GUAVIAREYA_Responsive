d6f790a0762b0242cbeb901672ec96a965f4a2b2
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-08-01 16:10:18 -0500

    Delete JS directory

--- JS/mostrar_contraseña.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('mostrarContrasena').addEventListener('change', function() {
-    var contrasenaInput = document.getElementById('Contrasena');
-    if (this.checked) {
-        contrasenaInput.type = 'text';
-    } else {
-        contrasenaInput.type = 'password';
-    }
-});
