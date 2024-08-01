968b484790f1d5eb3d0cd190eb26ea65188a3931
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2024-08-01 17:10:17 -0500

    Update GUAVIAREYA_CORREGIDO.sql

--- GUAVIAREYA_CORREGIDO.sql
+++ GUAVIAREYA_CORREGIDO.sql
@@ -92,9 +92,10 @@ CREATE TABLE Pedidos (
     ID_Producto INT NOT NULL,
     cantidad INT NOT NULL,
     Sub_total DOUBLE NOT NULL,
-	ID_Dire_Entre INT NOT NULL,
+    ID_Dire_Entre INT NOT NULL,
     Correo VARCHAR(50) NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Estado ENUM('Pendiente', 'Enviado', 'Entregado', 'Cancelado') NOT NULL DEFAULT 'Pendiente',
     CONSTRAINT FK_Productos_Pedidos FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto),
 	CONSTRAINT FK_Restaurantes_Pedidos FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante)
 );
