2405ce54282200d1698f7064057e25b1a4686580
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-08-01 20:23:01 -0500

    Add files via upload

--- GUAVIAREYA_CORREGIDO.sql
+++ GUAVIAREYA_CORREGIDO.sql
@@ -1,6 +1,6 @@
+-- Crear el esquema
 CREATE SCHEMA bd_guaviareya;
 USE bd_guaviareya;
-
 
 -- Tabla Usuarios
 CREATE TABLE Usuarios (
@@ -8,11 +8,12 @@ CREATE TABLE Usuarios (
     Apodo VARCHAR(50) NOT NULL,
     Nombre VARCHAR(50) NOT NULL,
     Apellido VARCHAR(50) NOT NULL,
-    Contrasena VARCHAR(50) NOT NULL,
+    Contrasena VARCHAR(255) NOT NULL,  -- Aumentado para mayor longitud
     Telefono VARCHAR(15) NOT NULL,
-    Fec_Regis  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    Fec_Regis TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     img_U VARCHAR(200) NOT NULL
 );
+
 -- Tabla Restaurantes
 CREATE TABLE Restaurantes (
     ID_Restaurante INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -20,27 +21,22 @@ CREATE TABLE Restaurantes (
     Direccion VARCHAR(50) NOT NULL,
     Telefono VARCHAR(15) NOT NULL,
     img_R VARCHAR(200) NOT NULL,
-	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Estado VARCHAR(15) NOT NULL
 );
--- Agregar columna Estado a la tabla Restaurantes
-ALTER TABLE Restaurantes 
-ADD COLUMN Estado VARCHAR(15) NOT NULL;
--- Tabla Administrador
-CREATE TABLE administradores (
+
+-- Tabla Administradores
+CREATE TABLE Administradores (
     id INT AUTO_INCREMENT PRIMARY KEY,
     correo VARCHAR(50) NOT NULL UNIQUE,
     apodo VARCHAR(50) NOT NULL,
     ID_Restaurante INT NULL,
-    contrasena VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,  -- Aumentado para mayor longitud
     rol ENUM('administrador', 'super_administrador') NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     img_A VARCHAR(200) NULL,
     CONSTRAINT FK_Restaurantes_Administradores FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante)
 );
-
-INSERT INTO administradores (correo, apodo, contrasena, rol)
-VALUES ('guaviareya@gmail.com', 'GuaviareYa', '12345678Aa@', 'super_administrador');
-
 
 -- Tabla Productos
 CREATE TABLE Productos (
@@ -53,26 +49,25 @@ CREATE TABLE Productos (
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_Restaurantes_Productos FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante)
 );
+
+-- Crear índice en la columna ID_Restaurante para mejorar el rendimiento de las búsquedas
+CREATE INDEX idx_ID_Restaurante ON Productos (ID_Restaurante);
+
 -- Tabla Direccion_Entregas
 CREATE TABLE Direccion_Entregas (
     ID_Dire_Entre INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Correo VARCHAR(50) NOT NULL,
-    Direccion VARCHAR(15) NOT NULL,
+    Direccion VARCHAR(100) NOT NULL,  -- Aumentado para permitir direcciones más largas
     Barrio VARCHAR(50) NOT NULL,
     fecha_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     Descripcion VARCHAR(50) NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_Usuarios_Direccion_Entregas FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
 );
--- Tabla Domiciliarios
-CREATE TABLE Domiciliarios (
-    ID_Domiciliario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    Correo VARCHAR(50) NOT NULL,
-    Nombre_Domiciliario VARCHAR(20) NOT NULL,
-    Telefono_Domi VARCHAR(15) NOT NULL,
-    img_do VARCHAR(200) NOT NULL,
-    CONSTRAINT FK_Usuarios_Domiciliarios FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
-);
+
+-- Crear índice en la columna Correo para mejorar el rendimiento de las búsquedas
+CREATE INDEX idx_Correo ON Direccion_Entregas (Correo);
+
 -- Tabla metodos_pago
 CREATE TABLE metodos_pago (
     id_pago INT AUTO_INCREMENT PRIMARY KEY,
@@ -83,8 +78,12 @@ CREATE TABLE metodos_pago (
     cvv VARCHAR(3) NOT NULL,
     correo VARCHAR(50) NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_usuarios_metodos_pago FOREIGN KEY (correo) REFERENCES usuarios (correo)
+    CONSTRAINT fk_usuarios_metodos_pago FOREIGN KEY (correo) REFERENCES Usuarios (Correo)
 );
+
+-- Crear índice en la columna correo para mejorar el rendimiento de las búsquedas
+CREATE INDEX idx_correo_metodos_pago ON metodos_pago (correo);
+
 -- Tabla Pedidos
 CREATE TABLE Pedidos (
     ID_pedido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -95,10 +94,16 @@ CREATE TABLE Pedidos (
     ID_Dire_Entre INT NOT NULL,
     Correo VARCHAR(50) NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Estado ENUM('Pendiente', 'Enviado', 'Entregado', 'Cancelado') NOT NULL DEFAULT 'Pendiente',
     CONSTRAINT FK_Productos_Pedidos FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto),
-	CONSTRAINT FK_Restaurantes_Pedidos FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante)
+    CONSTRAINT FK_Restaurantes_Pedidos FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante),
+    CONSTRAINT FK_Direccion_Entregas_Pedidos FOREIGN KEY (ID_Dire_Entre) REFERENCES Direccion_Entregas (ID_Dire_Entre),
+    CONSTRAINT FK_Usuarios_Pedidos FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
 );
+
+-- Crear índices en columnas que se usan en las búsquedas
+CREATE INDEX idx_ID_Restaurante_Pedidos ON Pedidos (ID_Restaurante);
+CREATE INDEX idx_ID_Producto_Pedidos ON Pedidos (ID_Producto);
+CREATE INDEX idx_Correo_Pedidos ON Pedidos (Correo);
 
 -- Tabla Pedidos_factura
 CREATE TABLE Pedidos_factura (
@@ -109,7 +114,6 @@ CREATE TABLE Pedidos_factura (
     ID_Producto INT NOT NULL,
     ID_Dire_Entre INT NOT NULL,
     id_pago INT NOT NULL,
-    ID_Domiciliario INT NOT NULL,
     Estado_Pedido VARCHAR(50) NOT NULL,
     Subtotal INT NOT NULL,
     Valor_Domi INT NOT NULL DEFAULT 5000,
@@ -118,10 +122,17 @@ CREATE TABLE Pedidos_factura (
     CONSTRAINT FK_Usuarios_Pedidos_factura FOREIGN KEY (Correo) REFERENCES Usuarios (Correo),
     CONSTRAINT FK_Direccion_Entregas_Pedidos_factura FOREIGN KEY (ID_Dire_Entre) REFERENCES Direccion_Entregas (ID_Dire_Entre),
     CONSTRAINT FK_Productos_Pedidos_factura FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto),
-    CONSTRAINT FK_Domiciliarios_Pedidos_factura FOREIGN KEY (ID_Domiciliario) REFERENCES Domiciliarios (ID_Domiciliario),
     CONSTRAINT FK_Restaurantes_Pedidos_factura FOREIGN KEY (ID_Restaurante) REFERENCES Restaurantes (ID_Restaurante),
     CONSTRAINT FK_Pagos_Pedidos_factura FOREIGN KEY (id_pago) REFERENCES metodos_pago (id_pago)
 );
+
+-- Crear índices en columnas que se usan en las búsquedas
+CREATE INDEX idx_Correo_Pedidos_factura ON Pedidos_factura (Correo);
+CREATE INDEX idx_ID_Restaurante_Pedidos_factura ON Pedidos_factura (ID_Restaurante);
+CREATE INDEX idx_ID_Producto_Pedidos_factura ON Pedidos_factura (ID_Producto);
+CREATE INDEX idx_ID_Dire_Entre_Pedidos_factura ON Pedidos_factura (ID_Dire_Entre);
+CREATE INDEX idx_id_pago_Pedidos_factura ON Pedidos_factura (id_pago);
+
 -- Tabla de cupones
 CREATE TABLE Cupones (
     ID_Cupon INT AUTO_INCREMENT PRIMARY KEY,
@@ -134,9 +145,10 @@ CREATE TABLE Cupones (
     CONSTRAINT FK_Usuarios_Cupones FOREIGN KEY (Correo) REFERENCES Usuarios (Correo)
 );
 
+-- Crear índice en la columna Correo para mejorar el rendimiento de las búsquedas
+CREATE INDEX idx_Correo_Cupones ON Cupones (Correo);
 
-
-
+-- Trigger para generar un cupón después de insertar un usuario
 DELIMITER //
 
 CREATE TRIGGER after_usuario_insert
@@ -150,12 +162,59 @@ BEGIN
     -- Generar un código de cupón único
     SET cupon_codigo = CONCAT('CUPON', NEW.Correo, UNIX_TIMESTAMP());
     -- Establecer el descuento y la fecha de expiración
-    SET cupon_descuento = 10; -- Descuento del 10%, por ejemplo
+    SET cupon_descuento = 10; -- Descuento del 10%
     SET cupon_expiracion = DATE_ADD(CURRENT_DATE, INTERVAL 30 DAY); -- 30 días de validez
 
     -- Insertar el nuevo cupón en la tabla Cupones
     INSERT INTO Cupones (Correo, Codigo_Cupon, Descuento, Fecha_Expiracion)
     VALUES (NEW.Correo, cupon_codigo, cupon_descuento, cupon_expiracion);
+END;
+//
+
+DELIMITER ;
+
+-- Procedimiento almacenado para insertar un administrador
+DELIMITER //
+
+CREATE PROCEDURE insertar_administrador(
+    IN p_correo VARCHAR(50),
+    IN p_apodo VARCHAR(50),
+    IN p_contrasena VARCHAR(255),
+    IN p_rol ENUM('administrador', 'super_administrador'),
+    IN p_ID_Restaurante INT,
+    IN p_img_A VARCHAR(200)
+)
+BEGIN
+    INSERT INTO Administradores (correo, apodo, contrasena, rol, ID_Restaurante, img_A)
+    VALUES (p_correo, p_apodo, p_contrasena, p_rol, p_ID_Restaurante, p_img_A);
+END;
+//
+
+DELIMITER ;
+
+-- Llamado del procedimiento almacenado
+CALL insertar_administrador(
+    'guaviareya@gmail.com',
+    'GuaviareYa',
+    '12345678Aa@',
+    'super_administrador',
+    NULL, -- Si no hay restaurante asociado
+    NULL  -- Si no hay imagen asociada
+);
+
+-- Función para verificar el estado del restaurante
+DELIMITER //
+
+CREATE FUNCTION verificar_estado_restaurante(id_restaurante INT) 
+RETURNS VARCHAR(15)
+BEGIN
+    DECLARE estado VARCHAR(15);
+    
+    SELECT Estado INTO estado
+    FROM Restaurantes
+    WHERE ID_Restaurante = id_restaurante;
+    
+    RETURN estado;
 END;
 //
 
@@ -167,11 +226,7 @@ SELECT * FROM Restaurantes;
 SELECT * FROM administradores;
 SELECT * FROM Productos;
 SELECT * FROM Direccion_Entregas;
-SELECT * FROM Domiciliarios;
 SELECT * FROM metodos_pago;
 SELECT * FROM Pedidos;
 SELECT * FROM Pedidos_factura;
 SELECT * FROM Cupones;
-
-
-
