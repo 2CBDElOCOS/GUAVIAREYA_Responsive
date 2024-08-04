ab51cd70c2deb79178fd7cc3322666374dd5a2dd
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-08-04 12:10:51 -0500

    Add files via upload

--- PAGINA GUAVIAREYA!/Modelos/Conexion.php
+++ PAGINA GUAVIAREYA!/Modelos/Conexion.php
@@ -2,17 +2,13 @@
 /**
  * Función para establecer una conexión con la base de datos
  *
- * Esta función crea y retorna un objeto de conexión mysqli utilizando los parámetros especificados.
- *
+ * @param string $servername Dirección del servidor
+ * @param string $username Nombre de usuario de la base de datos
+ * @param string $password Contraseña de la base de datos
+ * @param string $dbname Nombre de la base de datos
  * @return mysqli|false Retorna el objeto de conexión mysqli si la conexión es exitosa, o false si falla.
  */
-function Conexion() {
-    // Definir los parámetros de la conexión
-    $servername = "127.0.0.1"; // Dirección del servidor
-    $username = "root";        // Nombre de usuario de la base de datos
-    $password = "";            // Contraseña de la base de datos
-    $dbname = "bd_guaviareya"; // Nombre de la base de datos
-
+function Conexion($servername, $username, $password, $dbname) {
     // Crear una nueva conexión usando los parámetros definidos
     $conn = new mysqli($servername, $username, $password, $dbname);
 
