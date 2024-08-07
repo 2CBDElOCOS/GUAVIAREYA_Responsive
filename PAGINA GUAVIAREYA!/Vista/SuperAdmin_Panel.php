15313fa48af44be90a43950d6df5e5a588d59f2d
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-08-07 13:13:05 -0500

    Add files via upload

--- PAGINA GUAVIAREYA!/Vista/SuperAdmin_Panel.php
+++ PAGINA GUAVIAREYA!/Vista/SuperAdmin_Panel.php
@@ -1,5 +1,18 @@
-<head>
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+if (!isset($_SESSION['correo']) || $_SESSION['correo'] == "") {
+    header("location: ../Controladores/controlador.php?seccion=login");
+    exit();
+}?>
+
+<head>
   <title><?php $seccion = 'SuperAdmin_Panel';
   echo $seccion; ?></title>
 </head>
