f0d87758df0e260b62d441e80c6415515fc06bfd
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-08-07 14:25:52 -0500

    Add files via upload

--- PAGINA GUAVIAREYA!/Controladores/Controlador_Registrar.php
+++ PAGINA GUAVIAREYA!/Controladores/Controlador_Registrar.php
@@ -5,13 +5,10 @@ if (session_status() == PHP_SESSION_NONE) {
 
 require_once("../Modelos/Registrar.php");
 
-// Verificar si se han enviado los datos del formulario de registro
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verificar el CAPTCHA de Cloudflare
     $captchaResponse = $_POST['cf-turnstile-response'];
-    $secretKey = '0x4AAAAAAAgDs52B4mxcqN8Dogf2JgT5KTg'; // Reemplaza con tu clave secreta
+    $secretKey = '0x4AAAAAAAgDs52B4mxcqN8Dogf2JgT5KTg';
 
-    // Crear la solicitud para verificar el CAPTCHA
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://challenges.cloudflare.com/turnstile/v0/siteverify");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -26,27 +23,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $verification = json_decode($response);
 
     if ($verification->success) {
-        // El CAPTCHA fue verificado correctamente
         $result = Registrar::registrarUsuario();
         
         if ($result === true) {
-            // Registro exitoso
             header("location: ../Controladores/controlador.php?seccion=login");
             exit();
         } else {
-            // Error al registrar el usuario
-            $error = $result; // Captura el mensaje de error
-            header("location: controlador.php?seccion=registro&error=1");
+            $error = ($result === "Usuario ya existente") ? 1 : 3; // 3 = Error general
+            header("location: controlador.php?seccion=registro&error=$error");
             exit();
         }
     } else {
-        // El CAPTCHA falló
         header("location: controlador.php?seccion=registro&error=2");
         exit();
     }
 } else {
-    // Cargar la vista de registro si no se han enviado datos
     header("location: ../Controladores/controlador.php?seccion=registro");
     exit();
 }
-?>
+
