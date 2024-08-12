b48552878bac5e41a56715a4e24e5574767e9d7f
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2024-08-12 18:56:21 -0500

    Add files via upload

--- PAGINA GUAVIAREYA!/Vista/confirmacion.php
+++ PAGINA GUAVIAREYA!/Vista/confirmacion.php
@@ -2,24 +2,26 @@
 <html lang="en">
 
 <head>
-  <title>GuaviareYa!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuaviareYa!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
-<body>
-    <div class="container">
-        <div class="col-md-12 ico-carro">
-            <a href="controlador.php?seccion=shop"><i class="bx bxs-home"></i></a>
+<body class="bg-light d-flex flex-column min-vh-100">
+    <div class="container my-4">
+    <div class="ico-carro">
+            <a href="controlador.php?seccion=shop" class="btn btn-link">
+                <i class="bx bxs-home"></i>
+            </a>
         </div>
 
-        <div class="subcontainer4">
-            <h1 style="text-align: center;">Gracias por tu compra</h1>
-            <center><img src="../media/check.png" alt="listo" width="400px"></center>
-            
+        <div class="d-flex flex-column align-items-center">
+            <h1 class="mb-4">Gracias por tu compra</h1>
+            <img src="../media/check.png" alt="listo" class="img-fluid" style="max-width: 400px;">
         </div>
     </div>
-
-
-
 </body>
 
 </html>
